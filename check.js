40df32e74a93fc348c228f7e6f121d214772b22e
Kalys Kukanov <kalysbek.kukanov@gmail.com>
2021-11-20 16:18:07 +0600

    DZ_1

---
+++ check.js
@@ -0,0 +1,8 @@
+const arr1 = [1, 2, 5, 3, 6]
+const arr2 = [23, 2, 54, 0, 9, 87]
+
+if (arr1.length < arr2.length) {
+  console.log("arr2 больше чем arr1")
+} else {
+  console.log("arr1 больше чем arr2")
+}
---
+++ svetofor.js
@@ -0,0 +1,23 @@
+let resText = () => {
+    color === 'green' ? console.log("can go") :
+        color === 'yellow' ? console.log("be ready") :
+            color === 'red' ? console.log("stop") : console.log("no color!")
+}
+
+// let color = "green"
+//
+// switch (color) {
+//     case "green":
+//         console.log("can go");
+//         break;
+//     case "yellow":
+//         console.log("be ready");
+//         break;
+//     case "red":
+//         console.log("stop!");
+//         break;
+//     default:
+//         console.log("no color!")
+//         break;
+// }
+
